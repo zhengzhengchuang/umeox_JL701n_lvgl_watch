e42c2627b4c123c0ab129100cfd58ff10fb0ae1e
zhengzhengchuang <1811253744@qq.com>
2023-11-03 18:05:21 +0800

    修改分屏页面

--- code/sdk/apps/common/ui/lv_watch/ui_menu/split_screen/split_screen.c
+++ code/sdk/apps/common/ui/lv_watch/ui_menu/split_screen/split_screen.c
@@ -5,7 +5,6 @@ static lv_anim_t split_screen_anim;
 static bool is_split_screen = false; //是否拉出分屏
 static lv_obj_t *tileview_menu = NULL;
 static int16_t split_screen_dela_x = 0;
-static bool split_screen_state = false; //分屏状态是否完成
 static lv_obj_t *split_screen_container = NULL;
 
 static void split_screen_anim_cb(void *var, int32_t anim_val)
@@ -21,20 +20,18 @@ static void split_screen_anim_ready_cb(lv_anim_t *a)
 {
     int16_t lcd_w = lv_disp_get_hor_res(NULL);
 
-    lv_obj_t *parent = (lv_obj_t *)lv_anim_get_user_data(a);
+    lv_obj_t *tileview_menu = (lv_obj_t *)lv_anim_get_user_data(a);
 
     printf("%s:split_screen_dela_x = %d\n", __func__, split_screen_dela_x);
 
     if(split_screen_dela_x <= 0)
     {
         split_screen_dela_x = 0;
-        split_screen_state = false;
-        lv_obj_add_flag(parent, LV_OBJ_FLAG_SCROLLABLE);
+        lv_obj_add_flag(tileview_menu, LV_OBJ_FLAG_SCROLLABLE);
     }else if(split_screen_dela_x >= lcd_w/2)
     {
-        split_screen_state = true;
         split_screen_dela_x = lcd_w/2;   
-        lv_obj_clear_flag(parent, LV_OBJ_FLAG_SCROLLABLE);
+        lv_obj_clear_flag(tileview_menu, LV_OBJ_FLAG_SCROLLABLE);
     }
 
     return;
@@ -44,20 +41,24 @@ static void split_screen_pressed_cb(lv_event_t *e)
 {
     lv_point_t act_p;
 
-    lv_obj_t *obj = lv_event_get_target(e);
-    lv_obj_t *parent = lv_obj_get_parent(obj);
-
     int16_t lcd_w = lv_disp_get_hor_res(NULL);
 
     lv_indev_get_point(lv_indev_get_act(), &act_p);
 
-    if(act_p.x < lcd_w/10)
+    if(lv_obj_has_flag(tileview_menu, LV_OBJ_FLAG_SCROLLABLE))
+    {
+        if(act_p.x < lcd_w/10)
+            is_split_screen = true;
+        else
+            is_split_screen = false;
+    }else
         is_split_screen = true;
-    else
-        is_split_screen = false;
+    
 
     if(is_split_screen)
-        lv_obj_clear_flag(parent, LV_OBJ_FLAG_SCROLLABLE);
+        lv_obj_clear_flag(tileview_menu, LV_OBJ_FLAG_SCROLLABLE);
+
+    printf("%s\n", __func__);
         
     return;
 }
@@ -74,21 +75,24 @@ static void split_screen_pressing_cb(lv_event_t *e)
 
     split_screen_dela_x += vert_p.x;
 
+    if(split_screen_dela_x > 0 && split_screen_dela_x <= lcd_w/2)
+    {
+        if(split_screen_container)
+            lv_obj_set_style_translate_x(split_screen_container, split_screen_dela_x, LV_PART_MAIN);
+    }else
+    {
+        split_screen_dela_x = split_screen_dela_x < 0?0:split_screen_dela_x;
+        split_screen_dela_x = split_screen_dela_x > lcd_w/2?lcd_w/2:split_screen_dela_x;
+    } 
+#if 0
     if(!split_screen_state)
     {
-        if(split_screen_dela_x > 0 && split_screen_dela_x <= lcd_w/2)
-        {
-            if(split_screen_container)
-                lv_obj_set_style_translate_x(split_screen_container, split_screen_dela_x, LV_PART_MAIN);
-        }else
-        {
-            split_screen_dela_x = split_screen_dela_x < 0?0:split_screen_dela_x;
-            split_screen_dela_x = split_screen_dela_x > lcd_w/2?lcd_w/2:split_screen_dela_x;
-        } 
+        
     }else
     {
 
     }
+#endif
 
     return;
 }
@@ -99,27 +103,59 @@ static void split_screen_released_cb(lv_event_t *e)
 
     lv_point_t vert_p;
     
-    lv_obj_t *obj = lv_event_get_target(e);
-    lv_obj_t *parent = lv_obj_get_parent(obj);
-
     int16_t lcd_w = lv_disp_get_hor_res(NULL);
 
     lv_indev_get_vect(lv_indev_get_act(), &vert_p);
 
     printf("%s:split_screen_dela_x = %d\n", __func__, split_screen_dela_x);
 
+    if(split_screen_dela_x <= 0)
+    {
+        split_screen_dela_x = 0;
+        lv_obj_add_flag(tileview_menu, LV_OBJ_FLAG_SCROLLABLE);
+    }else if(split_screen_dela_x >= lcd_w/2)
+    {
+        split_screen_dela_x = lcd_w/2;   
+        lv_obj_clear_flag(tileview_menu, LV_OBJ_FLAG_SCROLLABLE);
+    }else
+    {
+        widget_anim_para.anim = &split_screen_anim;
+        widget_anim_para.anim_obj = split_screen_container;
+        widget_anim_para.anim_duration = 200;
+        widget_anim_para.anim_start_val = split_screen_dela_x + vert_p.x;
+
+        if(vert_p.x < 0)
+            widget_anim_para.anim_end_val = 0;
+        else if(vert_p.x > 0)
+            widget_anim_para.anim_end_val = lcd_w/2;
+        else
+        {
+            if(split_screen_dela_x >= lcd_w/4)
+                widget_anim_para.anim_end_val = lcd_w/2;
+            else
+                widget_anim_para.anim_end_val = 0; 
+        }
+
+        widget_anim_para.anim_exec_xcb = split_screen_anim_cb;
+        widget_anim_para.is_start_anim = false;
+        common_widget_anim_create(&widget_anim_para);
+        lv_anim_set_user_data(&split_screen_anim, (void *)tileview_menu);
+        lv_anim_set_ready_cb(&split_screen_anim, split_screen_anim_ready_cb);
+        lv_anim_start(&split_screen_anim);
+    }
+#if 0
     if(!split_screen_state)
     {
         if(split_screen_dela_x <= 0)
         {
             split_screen_dela_x = 0;
             split_screen_state = false;
-            lv_obj_add_flag(parent, LV_OBJ_FLAG_SCROLLABLE);
+            lv_obj_add_flag(tileview_menu, LV_OBJ_FLAG_SCROLLABLE);
         }else if(split_screen_dela_x >= lcd_w/2)
         {
             split_screen_state = true;
             split_screen_dela_x = lcd_w/2;   
-            lv_obj_clear_flag(parent, LV_OBJ_FLAG_SCROLLABLE);
+            lv_obj_clear_flag(tileview_menu, LV_OBJ_FLAG_SCROLLABLE);
         }else
         {
             widget_anim_para.anim = &split_screen_anim;
@@ -146,6 +182,7 @@ static void split_screen_released_cb(lv_event_t *e)
             lv_anim_start(&split_screen_anim);
         }
     }
+#endif
 
     return;
 }
@@ -171,7 +208,7 @@ void menu_split_screen_create(lv_obj_t *obj)
     if(!obj) return;
 
     is_split_screen = false;
-    split_screen_state = false;
+    split_screen_dela_x = 0;
 
     int16_t lcd_w = lv_disp_get_hor_res(NULL);
     int16_t lcd_h = lv_disp_get_ver_res(NULL);
